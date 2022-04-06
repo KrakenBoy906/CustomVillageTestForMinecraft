6530731a7148d30a63a27d0f3249e84d2891c63f
KrakenBoy906 <ShouryaPaul6@gmail.com>
2022-04-06 16:23:03 +0530

    Little adding

---
+++ datatypes/Color.java
@@ -0,0 +1,14 @@
+package com.shourya.customvillage.datatypes;
+
+public class Color
+{
+	public int RED;
+	public int GREEN;
+	public int BLUE;
+	
+	public Color(int r, int g, int b) {
+		this.RED = r;
+		this.GREEN = g;
+		this.BLUE = b;
+	}
+}
